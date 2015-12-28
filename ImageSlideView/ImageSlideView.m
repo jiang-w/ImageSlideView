f29e318cc91d9b354dd62f9ca84a5f0863ac6b64
jiang-w <frank@iMac-bigdata.local>
2015-12-28 14:10:45 +0800

    commit

--- ImageSlideView/ImageSlideView.m
+++ ImageSlideView/ImageSlideView.m
@@ -12,8 +12,8 @@
 @interface ImageSlideView()
 
 @property(nonatomic, strong) UIScrollView *imageScroll;
-@property(nonatomic, strong) UIView *container;
 @property(nonatomic, strong) NSMutableArray<UIImageView *> *imageViews;
+@property(nonatomic, weak) NSTimer *timer;
 
 @end
 
@@ -22,68 +22,49 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        [self initView];
-        
-        [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(revolvingScroll) userInfo:nil repeats:YES];
+        [self addSubview:self.imageScroll];
     }
     return self;
 }
 
 - (void)layoutSubviews {
-    if (!CGSizeEqualToSize(self.imageScroll.contentSize, CGSizeZero)) {
-        if (self.images.count > 1) {
-            UIImageView *firstImage = self.imageViews[1];
-            self.imageScroll.contentOffset = firstImage.frame.origin;
-        }
+    if (self.images.count > 1 && !CGSizeEqualToSize(self.imageScroll.contentSize, CGSizeZero)) {
+        UIImageView *firstImage = self.imageViews[1];
+//        NSLog(@"image frame: %@", NSStringFromCGRect(firstImage.frame));
+        self.imageScroll.contentOffset = firstImage.frame.origin;
     }
 }
 
-
-#pragma mark - private
-
-- (void)initView {
-    [self addSubview:self.imageScroll];
-    [self.imageScroll mas_makeConstraints:^(MASConstraintMaker *make) {
+- (void)updateConstraints {
+    [self.imageScroll mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.edges.equalTo(self);
     }];
     
-    [self.imageScroll addSubview:self.container];
-    [self.container mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.equalTo(self.imageScroll);
-        make.height.equalTo(self.imageScroll);
-    }];
-    
-//    UIView *frame = [UIView new];
-//    [self.imageScroll addSubview:frame];
-//    frame.layer.borderColor = [UIColor redColor].CGColor;
-//    frame.layer.borderWidth = 2.0f;
-//    [frame mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.edges.equalTo(self.imageScroll);
-//    }];
-}
-
-- (void)setImageViewAndLayout {
     for (int i = 0; i < self.imageViews.count; i++) {
         UIImageView *imgView = self.imageViews[i];
-        [self.container addSubview:imgView];
-        [imgView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.width.equalTo(self.imageScroll);
+        [imgView mas_remakeConstraints:^(MASConstraintMaker *make) {
+            make.width.height.equalTo(self.imageScroll);
+            make.top.bottom.equalTo(self.imageScroll);
             
             if (i == 0) {
-                make.left.top.bottom.equalTo(self.container);
+                make.left.equalTo(self.imageScroll);
             }
             else {
                 UIImageView *pervImgView = self.imageViews[i - 1];
                 make.left.equalTo(pervImgView.mas_right);
-                make.top.bottom.equalTo(self.container);
             }
-
+            
             if (i == self.imageViews.count - 1) {
-                make.right.equalTo(self.container);
+                make.right.equalTo(self.imageScroll);
             }
         }];
     }
+    
+    [super updateConstraints];
 }
+
+
+#pragma mark - private
 
 - (void)clearAllImages {
     for (UIImageView *view in self.imageViews) {
@@ -94,12 +75,14 @@
 
 - (void)revolvingScroll {
     CGFloat pageWidth = CGRectGetWidth(self.imageScroll.frame);
-    CGPoint offset = self.imageScroll.contentOffset;
-    [self.imageScroll setContentOffset:CGPointMake(offset.x + pageWidth, 0) animated:YES];
-    
-    if (offset.x == pageWidth * (self.imageViews.count - 1)) {
-        self.imageScroll.contentOffset = CGPointMake(pageWidth, 0);
+    CGFloat xOffset = self.imageScroll.contentOffset.x;
+    if (xOffset == self.imageScroll.contentSize.width - pageWidth * 2) {
+        xOffset = 0;
+        [self.imageScroll setContentOffset:CGPointMake(xOffset, 0) animated:NO];
     }
+    NSLog(@"xOffset begin: %f", xOffset);
+    [self.imageScroll setContentOffset:CGPointMake(xOffset + pageWidth, 0) animated:YES];
+    NSLog(@"xOffset begin: %f", xOffset + pageWidth);
 }
 
 
@@ -114,20 +97,10 @@
         _imageScroll.bounces = NO;
         _imageScroll.delegate = self;
         
-//        _imageScroll.backgroundColor = [UIColor lightGrayColor];
+        _imageScroll.backgroundColor = [UIColor lightGrayColor];
 //        _imageScroll.clipsToBounds = NO;
     }
     return _imageScroll;
-}
-
-- (UIView *)container {
-    if (!_container) {
-        _container = [[UIView alloc] init];
-        
-//        _container.backgroundColor = [UIColor blueColor];
-//        _container.alpha = 0.5;
-    }
-    return _container;
 }
 
 - (NSMutableArray *)imageViews {
@@ -139,21 +112,32 @@
 
 - (void)setImages:(NSArray<UIImage *> *)images {
     if (_images != images) {
+        if (self.timer) {
+            [self.timer invalidate];
+        }
+        
         _images = images;
         [self clearAllImages];
         
         for (UIImage *img in _images) {
             UIImageView *imageView = [[UIImageView alloc] initWithImage:img];
             [self.imageViews addObject:imageView];
+            [self.imageScroll addSubview:imageView];
         }
         if (_images.count > 1) {
             UIImageView *leading = [[UIImageView alloc] initWithImage:[_images lastObject]];
             [self.imageViews insertObject:leading atIndex:0];
+            [self.imageScroll addSubview:leading];
             UIImageView *trailing = [[UIImageView alloc] initWithImage:[_images firstObject]];
             [self.imageViews addObject:trailing];
+            [self.imageScroll addSubview:trailing];
         }
         
-        [self setImageViewAndLayout];
+        [self setNeedsUpdateConstraints];
+        
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:5
+                                                      target:self
+                                                    selector:@selector(revolvingScroll) userInfo:nil repeats:YES];
     }
 }
 
@@ -165,7 +149,7 @@
         CGPoint currentOffset = self.imageScroll.contentOffset;
         CGPoint firstImageOffset = self.imageViews[1].frame.origin;
         CGPoint lastImageOffset = self.imageViews[self.images.count].frame.origin;
-        
+
         if (CGPointEqualToPoint(currentOffset, [self.imageViews firstObject].frame.origin)) {
             self.imageScroll.contentOffset = lastImageOffset;
         }
@@ -173,6 +157,18 @@
             self.imageScroll.contentOffset = firstImageOffset;
         }
     }
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    if (self.timer) {
+        [self.timer invalidate];
+    }
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:5
+                                                  target:self
+                                                selector:@selector(revolvingScroll) userInfo:nil repeats:YES];
 }
 
 @end
